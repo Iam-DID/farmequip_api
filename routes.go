c4607f9ee6124bdee5ec7a3c96693ac824d71aab
Syadidch <syadidchoiri@gmail.com>
2025-11-28 12:05:39 +0700

    fix

--- routes.go
+++ routes.go
@@ -1,52 +1,54 @@
 package main
 
 import (
-	"database/sql"
-	"farmequip_api/handlers"
-	"net/http"
+    "database/sql"
+    "farmequip_api/handlers"
+    "net/http"
 
-	"github.com/gorilla/mux"
+    "github.com/gorilla/mux"
 )
 
-func enableCORS(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+func CorsMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-		next(w, r)
-	}
+        if r.Method == "OPTIONS" {
+            w.WriteHeader(http.StatusOK)
+            return
+        }
+
+        next.ServeHTTP(w, r)
+    })
 }
 
 func SetupRoutes(db *sql.DB) {
-	r := mux.NewRouter()
+    r := mux.NewRouter()
+    r.Use(CorsMiddleware)
 
-	// Login
-	r.HandleFunc("/login", enableCORS(handlers.Login(db))).Methods("POST")
+	//auth
+    r.HandleFunc("/login", handlers.Login(db)).Methods("POST")
 
-	// Users
-	r.HandleFunc("/users", enableCORS(handlers.UpdateUser(db))).Methods("PUT")
+	//user
+    r.HandleFunc("/users", handlers.UpdateUser(db)).Methods("PUT")
 
-	// Kategori
-	r.HandleFunc("/kategori", enableCORS(handlers.GetKategori(db))).Methods("GET")
-	r.HandleFunc("/kategori", enableCORS(handlers.CreateKategori(db))).Methods("POST")
-	r.HandleFunc("/kategori", enableCORS(handlers.UpdateKategori(db))).Methods("PUT")
-	r.HandleFunc("/kategori", enableCORS(handlers.DeleteKategori(db))).Methods("DELETE")
+	//kategori
+    r.HandleFunc("/kategori", handlers.GetKategori(db)).Methods("GET")
+    r.HandleFunc("/kategori", handlers.CreateKategori(db)).Methods("POST")
+    r.HandleFunc("/kategori", handlers.UpdateKategori(db)).Methods("PUT")
+    r.HandleFunc("/kategori", handlers.DeleteKategori(db)).Methods("DELETE")
 
-	// Alat
-	r.HandleFunc("/alat", enableCORS(handlers.GetAlat(db))).Methods("GET")
-	r.HandleFunc("/alat", enableCORS(handlers.CreateAlat(db))).Methods("POST")
-	r.HandleFunc("/alat", enableCORS(handlers.UpdateAlat(db))).Methods("PUT")
-	r.HandleFunc("/alat", enableCORS(handlers.DeleteAlat(db))).Methods("DELETE")
+	//alat
+    r.HandleFunc("/alat", handlers.GetAlat(db)).Methods("GET")
+    r.HandleFunc("/alat", handlers.CreateAlat(db)).Methods("POST")
+    r.HandleFunc("/alat", handlers.UpdateAlat(db)).Methods("PUT")
+    r.HandleFunc("/alat", handlers.DeleteAlat(db)).Methods("DELETE")
 
-	// Get by ID atau SLUG
-	r.HandleFunc("/alat/{id:[0-9]+}", enableCORS(handlers.GetToolById(db))).Methods("GET")
-	r.HandleFunc("/alat/{slug:[a-zA-Z0-9-_]+}", enableCORS(handlers.GetAlatBySlug(db))).Methods("GET")
+    // Detail alat
+    r.HandleFunc("/alat/{id:[0-9]+}", handlers.GetToolById(db)).Methods("GET")
+    r.HandleFunc("/alat/{slug:[a-zA-Z0-9-_]+}", handlers.GetAlatBySlug(db)).Methods("GET")
 
-	http.Handle("/", r)
+    http.Handle("/", r)
 }
